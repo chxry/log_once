e2d707fba2b35ba0d3ded54faf0d8eb5fe5aacd3
chxry <chxry@floppa.world>
2023-03-25 18:33:17 +0000

    based

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+pub use log;
+pub static mut H: Vec<u64> = vec![];
+
+#[macro_export]
+macro_rules! log_once {
+  ($lvl:expr, $($arg:tt)+) => (
+    unsafe {
+      struct T;
+      let id = ::std::mem::transmute::<_, u64>(::std::any::TypeId::of::<T>());
+      if !$crate::H.contains(&id) {
+        $crate::log::log!($lvl,$($arg)+);
+        $crate::H.push(id);
+      }
+    }
+  )
+}
+
+#[macro_export]
+macro_rules! error_once {
+  ($($arg:tt)+) => ($crate::log_once!($crate::log::Level::Error, $($arg)+))
+}
+
+#[macro_export]
+macro_rules! warn_once {
+  ($($arg:tt)+) => ($crate::log_once!($crate::log::Level::Warn, $($arg)+))
+}
+
+#[macro_export]
+macro_rules! info_once {
+  ($($arg:tt)+) => ($crate::log_once!($crate::log::Level::Info, $($arg)+))
+}
+
+#[macro_export]
+macro_rules! debug_once {
+  ($($arg:tt)+) => ($crate::log_once!($crate::log::Level::Debug, $($arg)+))
+}
+
+#[macro_export]
+macro_rules! trace_once {
+  ($($arg:tt)+) => ($crate::log_once!($crate::log::Level::Trace, $($arg)+))
+}
+
+#[test]
+fn it_works() {
+  ezlogger::init(log::LevelFilter::Info).unwrap();
+  info_once!("real");
+  for i in 0..3 {
+    info_once!("test {}", i);
+  }
+}
